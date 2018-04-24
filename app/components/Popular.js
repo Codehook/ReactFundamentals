bd7216cb8bc1694049c21a1a4334ecaea33b0ab7
Doylexan <briantrumpet3@gmail.com>
2018-04-24 15:15:14 -0500

    First commit
    First component with functionality

---
+++ app/components/Popular.js
@@ -0,0 +1,58 @@
+var React = require('react');
+var PropTypes = require('prop-types');
+
+function SelectedLanguage(props) {
+   var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+   
+   return (
+   <ul className='languages'>
+      {languages.map(function (lang) {
+      //console.log(this)
+         return(
+            <li 
+               style={lang === props.selectedLanguage ? {color: 'red'} : null}
+               onClick={props.onSelect.bind(null, lang)} 
+               key={lang}>
+               {lang}
+            </li>
+         )
+      })}
+   </ul>
+      )
+}
+
+SelectedLanguage.propTypes = {
+   selectedLanguage: PropTypes.string.isRequired,
+   onSelect: PropTypes.func.isRequired,
+}
+
+class Popular extends React.Component {
+   constructor(props) {
+      super(props);
+    
+      this.state = {
+        selectedLanguage: 'All',
+      };
+
+      this.updateLanguage = this.updateLanguage.bind(this);
+    }
+
+   updateLanguage(lang) {
+      this.setState({
+         selectedLanguage: lang
+      })
+    }
+
+   render() {
+      return(
+         <div>
+            <SelectedLanguage 
+               selectedLanguage={this.state.selectedLanguage}
+               onSelect={this.updateLanguage}
+            />
+         </div>   
+      )
+   }
+}
+            
+module.exports = Popular;
